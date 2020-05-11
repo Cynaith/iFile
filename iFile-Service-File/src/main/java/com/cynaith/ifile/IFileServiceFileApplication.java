ee73ce7554b079eb6c816c3b2a3419b27c633848
Cynaith <894324762@qq.com>
2020-05-11 23:22:28 +0800

    create iFile-Service-File

---
+++ iFile-Service-File/src/main/java/com/cynaith/ifile/IFileServiceFileApplication.java
@@ -0,0 +1,13 @@
+package com.cynaith.ifile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IFileServiceFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IFileServiceFileApplication.class, args);
+    }
+
+}
