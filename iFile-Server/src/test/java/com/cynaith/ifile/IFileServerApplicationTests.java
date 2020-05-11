005a5baed90f9f85522216a399f35b720ce8943d
Cynaith <894324762@qq.com>
2020-05-11 23:25:35 +0800

    Create IFileServerApplicationTests.java

---
+++ iFile-Server/src/test/java/com/cynaith/ifile/IFileServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cynaith.ifile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IFileServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
