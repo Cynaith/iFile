574d3f5b33df66c7da8030b8e93c7bc0c6f0fa67
Cynaith <894324762@qq.com>
2020-05-13 02:29:42 +0800

    Update UserController.java

--- iFile-Service-User/src/main/java/com/cynaith/ifile/controller/UserController.java
+++ iFile-Service-User/src/main/java/com/cynaith/ifile/controller/UserController.java
@@ -1,13 +1,17 @@
 package com.cynaith.ifile.controller;
 
-import com.cynaith.ifile.Redis.RedisUtil;
+import com.cynaith.ifile.util.GetRequestJsonUtils;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.net.ssl.SSLSessionContext;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.util.UUID;
 
 /**
@@ -17,15 +21,9 @@ import java.util.UUID;
 @RestController
 public class UserController {
 
-    @RequestMapping(value = "/login",method = RequestMethod.GET)
-    public String login(HttpServletRequest request, HttpServletResponse response){
-        String name = request.getParameter("iFile-username");
-        String password = request.getParameter("iFile-password");
-        String token = name+":"+password;
-        RedisUtil.set("token",token);
-        Cookie cookie = new Cookie("token",token);
-        response.addCookie(cookie);
-        return token;
+    @RequestMapping(value = "/login",method = RequestMethod.POST,produces = "application/json;charset=UTF-8")
+    public String login(HttpServletRequest request, HttpServletResponse response,@RequestBody String json){
+        return null;
     }
 
 }
