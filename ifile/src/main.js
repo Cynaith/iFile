5a13709e6cac4b77a34fa9bf14971fbd43e9f719
Cynaith <894324762@qq.com>
2020-07-10 23:35:51 +0800

    Update main.js

--- ifile/src/main.js
+++ ifile/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import ElementUI from 'element-ui';
 import axios from 'axios'
 import 'element-ui/lib/theme-chalk/index.css';
+axios.defaults.withCredentials = true
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 Vue.prototype.$axios = axios
